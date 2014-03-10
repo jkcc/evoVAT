1c87c0749bb3c176676b2cc70cfcb8309933453f
jkcc <jkccGh@gmail.com>
2014-03-11 01:12:35 +1100

    Updated cvat.cpp file, but still cannot output mst in sparse form.

--- matlab/mex/cvat.cpp
+++ matlab/mex/cvat.cpp
@@ -7,6 +7,8 @@
 
 #include <cmath>
 #include <cassert>
+#include <algorithm>
+#include <iostream>
 #include <vector>
 #include <list>
 #include "mex.h"
@@ -17,7 +19,13 @@
 
 void usageMessage();
 
-void primsAlgor(const double* pMDis, int rowNum, int colNum, double* pVPermVerts, double* pMMst);
+void primsAlgor(const double* pMDis, int rowNum, int colNum, double* pVPermVerts,
+        mxArray* mMst);
+
+
+typedef std::pair<int,int> C_IntPair;
+bool pairComparator ( const C_IntPair& l, const C_IntPair& r)
+   { return l.first < r.first; }
 
 
 // IMPLEMENATION
@@ -62,10 +70,9 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     // OUTPUT
     plhs[0] = mxCreateDoubleMatrix(1, rowNum, mxREAL);
     double* pVPermVerts = mxGetPr(plhs[0]);
-    plhs[1] = mxCreateSparse(rowNum, rowNum, 2*(rowNum-1), mxREAL);
-    double* pMMst = mxGetPr(plhs[1]);
+    plhs[1] = mxCreateSparse(rowNum, rowNum, rowNum-1, mxREAL);
 
-    primsAlgor(pMDis, rowNum, colNum, pVPermVerts, pMMst);
+    primsAlgor(pMDis, rowNum, colNum, pVPermVerts, plhs[1]);
 
 } // end of mexFunction()
 
@@ -74,20 +81,30 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 /* ************************************************************************* */
 
 
-void primsAlgor(const double* pMDis, int rowNum, int colNum, double* pVPermVerts, double* pMMst)
+void primsAlgor(const double* pMDis, int rowNum, int colNum, double* pVPermVerts,
+        mxArray* mMst)
 {
+    using std::cout;
+    using std::endl;
+    
+    double* pMMst = mxGetPr(mMst);
+    mwIndex* pMMstIr = mxGetIr(mMst);
+    mwIndex* pMMstJc = mxGetJc(mMst);    
     
     // find root vert and populate lIndices
     double maxDis = -1;
     int maxRow = -1;
     int maxCol = -1;
     for (int i = 0; i < rowNum * colNum; ++i) {
+//         cout << i << ": ";
         if (pMDis[i] > maxDis) {
+            maxDis = pMDis[i];
             // rowNum is the lenght of a column
             maxRow = i % rowNum; 
             maxCol = std::floor(i / rowNum);
+//             cout << "maxRow = " << maxRow << ", maxCol = " << maxCol;
         }
-        
+        cout << endl;
     }
     
     // choose maxRow as the starting vertex and remove it from lIndices
@@ -96,6 +113,7 @@ void primsAlgor(const double* pMDis, int rowNum, int colNum, double* pVPermVerts
     // intialise data structures
     typedef std::vector<double> C_Dis;
     typedef std::vector<int> C_VertIndices;
+    typedef std::vector<C_IntPair> C_PairIndices;
     typedef std::list<int> C_Indices;
     // vector of smallest distance to vertices not in mst
     C_Dis vDisRemaining = C_Dis(rowNum);
@@ -103,6 +121,9 @@ void primsAlgor(const double* pMDis, int rowNum, int colNum, double* pVPermVerts
     C_VertIndices vSrcIndices = C_VertIndices(rowNum);
     // list of indices to add to MST
     C_Indices lIndices = C_Indices();    
+    // edges in MST
+    C_VertIndices vMstSrcIndices = C_VertIndices(rowNum-1);
+    C_PairIndices vMstDestIndices = C_PairIndices(rowNum-1);
     
     for (int i = 0; i < rowNum; ++i) {
         lIndices.push_back(i);
@@ -140,6 +161,10 @@ void primsAlgor(const double* pMDis, int rowNum, int colNum, double* pVPermVerts
     justAddedVert = *itMin;
     pVPermVerts[permVertsCurrIndex] = justAddedVert + 1;
     ++permVertsCurrIndex;
+    int edgeIndex = 0;
+    vMstSrcIndices[edgeIndex] = srcIndex;
+    vMstDestIndices[edgeIndex] = std::make_pair(justAddedVert, edgeIndex);    
+    ++edgeIndex;
     lIndices.erase(itMin);
     
     // rest of vertices
@@ -171,11 +196,72 @@ void primsAlgor(const double* pMDis, int rowNum, int colNum, double* pVPermVerts
         justAddedVert = *itMin;
         pVPermVerts[permVertsCurrIndex] = justAddedVert + 1;
         ++permVertsCurrIndex;
+        vMstSrcIndices[edgeIndex] = srcIndex;
+        vMstDestIndices[edgeIndex] = std::make_pair(justAddedVert, edgeIndex);;        
+        ++edgeIndex;
         lIndices.erase(itMin);        
         
     } // end of while 
 
 
+    // update of MST
+    // sort the columns (destIndices)
+    std::sort(vMstDestIndices.begin(), vMstDestIndices.end(), pairComparator);
+    int currIndex = 0;
+    int currColIndex = 0;
+    mwIndex lastNZColIndex = 0;
+    std::cout << vMstDestIndices.size() << std::endl;
+    typename C_PairIndices::iterator itPairs  = vMstDestIndices.begin();
+    if ( itPairs != vMstDestIndices.end()) {
+        std::cout << "(" << vMstSrcIndices[itPairs->second] << ", " << itPairs->first << "), index = " << itPairs->second <<  std::endl;
+        while (currColIndex < itPairs->first) {
+            pMMstJc[currColIndex] = lastNZColIndex;
+            ++currColIndex;
+        }
+        
+        ++lastNZColIndex;
+        pMMstJc[currColIndex] = lastNZColIndex;
+        pMMstIr[currIndex] = vMstSrcIndices[itPairs->second];
+        pMMst[currIndex] = 1;
+        ++currIndex;
+        
+        ++itPairs;
+        
+        // do rest of pairs
+        for ( ; itPairs != vMstDestIndices.end(); ++itPairs) {
+            std::cout << "(" << vMstSrcIndices[itPairs->second] << ", " << itPairs->first << "), index = " << itPairs->second <<  std::endl;
+            while (itPairs->first != currColIndex) {
+                pMMstJc[currColIndex] = lastNZColIndex;
+                ++currColIndex;
+            }
+            
+            ++lastNZColIndex;
+            pMMstJc[currColIndex] = lastNZColIndex;
+            pMMstIr[currIndex] = vMstSrcIndices[itPairs->second];
+            pMMst[currIndex] = 1;
+            ++currIndex;
+        }
+    }
+    
+    
+    cout << "pMMstJc" << endl;
+    for (int j = 0; j < rowNum ; ++j) {
+        cout << pMMstJc[j] << ", ";
+    }
+    cout << endl;
+    
+    cout << "pMMstIr" << endl;
+    for (int j = 0; j < rowNum-1 ; ++j) {
+        cout << pMMstJc[j] << ", ";
+    }
+    cout << endl;
+    
+    cout << "pMMst" << endl;
+    for (int j = 0; j < rowNum-1 ; ++j) {
+        cout << pMMst[j] << ", ";
+    }    
+    cout << endl;
+    
 } // end of primsAlgor()
 
 
